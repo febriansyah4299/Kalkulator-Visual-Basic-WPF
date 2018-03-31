332cf070e922bbbb50de1c875a565955b76cce50
FEBRIANSYAH.ID <febriansyah4299@yahoo.com>
2018-03-31 20:13:54 +0700

    V0.1
    masih percobaan
    
    System.OverflowException error

---
+++ FEBRIANSYAHKalkulator/MainWindow.xaml.vb
@@ -0,0 +1,44 @@
+﻿Class MainWindow
+
+    Dim AngkaPertama As Long
+    Dim AngkaKedua As Long
+    Dim Kalkulasi As String
+    Dim Hasil As Long
+
+    Private Sub buttonTambah_Click(sender As Object, e As RoutedEventArgs) Handles buttonTambah.Click
+        Kalkulasi = "+"
+        labelOperasi.Content = "+"
+    End Sub
+
+    Private Sub buttonKurang_Click(sender As Object, e As RoutedEventArgs) Handles buttonKurang.Click
+        Kalkulasi = "-"
+        labelOperasi.Content = "-"
+    End Sub
+
+    Private Sub buttonKali_Click(sender As Object, e As RoutedEventArgs) Handles buttonKali.Click
+        Kalkulasi = "*"
+        labelOperasi.Content = "*"
+    End Sub
+
+    Private Sub buttonBagi_Click(sender As Object, e As RoutedEventArgs) Handles buttonBagi.Click
+        Kalkulasi = "/"
+        labelOperasi.Content = "/"
+    End Sub
+
+    Private Sub buttonSD_Click(sender As Object, e As RoutedEventArgs) Handles buttonSD.Click
+        AngkaPertama = textBox1.Text
+        AngkaKedua = textBox2.Text
+
+        If Kalkulasi = "+" Then
+            Hasil = AngkaPertama + AngkaKedua
+        ElseIf Kalkulasi = "-" Then
+            Hasil = AngkaPertama - AngkaKedua
+        ElseIf Kalkulasi = "*" Then
+            Hasil = AngkaPertama * AngkaKedua
+        ElseIf Kalkulasi = "/" Then
+            Hasil = AngkaPertama / AngkaKedua
+        End If
+
+        textBoxHasil.Text = Hasil
+    End Sub
+End Class
--- README.md
+++
@@ -1,1 +0,0 @@
-# Kalkulator-Visual-Basic-WPF
